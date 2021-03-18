236136a37f0c7ef858f5b2846650d94e084190cc
Cory Boudreaux <cboud31@gmail.com>
2021-03-18 16:44:02 -0500

    connect PostgreSQL DB, define tables architecture

---
+++ db/index.js
@@ -0,0 +1,11 @@
+const pg = require('pg');
+const client = new pg.Client('postgres://localhost:5432/jobTracker');
+
+// DB adapters?
+
+module.exports = {
+  client,
+  // DB Adapters,
+  // DB Adapters,
+  // DB Adapters,
+};
