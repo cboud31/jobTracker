d151f7dad5dc103cb5559f3e7b9781f6a7b852c0
Cory Boudreaux <cboud31@gmail.com>
2021-03-18 15:12:38 -0500

    Initial Express server setup

---
+++ server.js
@@ -0,0 +1,26 @@
+// This is the web server.
+const express = require('express');
+const server = express();
+
+// Access and load env variables
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+// CORS
+
+// Use morgan middleware to log all requests.
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+// Connect to the server.
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, async () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+
+  //   try {
+  //     await client.connect();
+  //     console.log('Connection to database is successful!');
+  //   } catch (error) {
+  //     console.error('Error connecting to database!');
+  //   }
+});
